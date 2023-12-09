110acf1b1e57fb382dab5065fc48974e95266a2e
11a <11a_git@proton.me>
2023-12-09 16:30:11 +0100

    Fixed Gtest and SFML implementation

---
+++ cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-src/googlemock
+# Build directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-src/googletest
+# Build directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
