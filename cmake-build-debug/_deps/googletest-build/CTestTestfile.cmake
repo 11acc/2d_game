d35458272040cd38611a51667f87a45515422b26
Alejandro Gutierrez Acosta <11a_git@proton.me>
2023-12-11 17:50:02 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-src
-# Build directory: C:/Users/aleja/Documents/Local Code/cp II/cz_assign/2d_game/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
