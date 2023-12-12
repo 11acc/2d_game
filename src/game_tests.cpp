7ba51046e292158feb250bf912f891c204cd75d3
11a <11a_git@proton.me>
2023-12-12 12:57:24 +0100

    working gtest file

---
+++ src/game_tests.cpp
@@ -0,0 +1,12 @@
+/*
+ * File: game_tests.cpp
+ * Author: Alejandro Gutierrez Acosta [11acc], Katherine Duque, Rama Mallela
+ * Date: Dec 11 2023
+ * Description: Test case functions for all parts of the game, using GTest
+ */
+
+#include <gtest/gtest.h>
+#include "game.h"
+#include "ghost.h"
+#include "fruit.h"
+
